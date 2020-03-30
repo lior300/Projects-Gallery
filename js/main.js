eb1bdb60a64490be3be942eee0c1e5a4d5feb907
lior300 <62352977+lior300@users.noreply.github.com>
2020-03-30 23:33:10 +0300

    Add peoject file

---
+++ js/main.js
@@ -0,0 +1,56 @@
+console.log('Starting up');
+
+$(document).ready(initPage)
+
+function initPage() {
+  renderGalleryProjects()
+}
+
+
+function renderGalleryProjects() {
+  var projects = GetgProjs()
+  var projectsHTMLs = projects.map(getProjectsGalleryHTML)
+  $('.profilesProjs').html(projectsHTMLs)
+}
+
+
+function getProjectsGalleryHTML(project) {
+  return `
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onClick="onProject(${project.id})">
+        <div class="portfolio-hover">
+          <div class="portfolio-hover-content">
+            <i class="fa fa-plus fa-3x"></i>
+          </div>
+        </div>
+        <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${project.name}</h4>
+          <p class="text-muted">${project.title}</p>
+      </div>
+    `
+}
+
+function onProject(idProject) {
+  var project = getProjectById(idProject)
+  var modalHTML = getProjectModalHTML(project)
+  $('.modal-body').html(modalHTML)
+}
+
+function getProjectModalHTML(project) {
+  return `
+    <h2>${project.name}</h2>
+    <p class="item-intro text-muted">${project.title}</p>
+    <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
+    <p>${project.desc}</p>
+    <ul class="list-inline">
+      <li>Date: ${project.publishedAt}</li>
+      <li>Category: ${project.labels.join(', ')}</li>
+    </ul>
+    <button class="btn btn-outline-warning" data-dismiss="modal" type="button">
+    <i class="fa fa-times"></i>
+    Close</button>
+    <a class="btn btn-outline-primary" href="${project.url}" target="_blank">Project Link</a>
+    `
+}
---
+++ js/proj-service.js
@@ -0,0 +1,71 @@
+
+var gId = 0
+var gProjs = createProjects();
+
+
+function createProjects() {
+    var projs = []
+    //Add Project1
+    projs.push(createProject(
+        'Minesweeper-The game',
+        '"minesweeper" - The game',
+        'with features and special bonuses',
+        ['Games'],
+        'projects/MineSweeper/index.html'))
+
+    //Add Project2
+    projs.push(createProject(
+        'What in the picture?',
+        'Guess what\'s in the picture game',
+        'A game where you get a picture and you have to answer what\'s in the picture',
+        ['Games'],
+        'projects/Ex-in-picture/index.html'))
+
+    //Add Project3
+    projs.push(createProject(
+        'Next-Number',
+        'Find the next number',
+        'A game where you have to find all the numbers in the fastest time',
+        ['Games'],
+        'projects/Ex-in-picture/index.html'))
+
+    //Add Project4
+    projs.push(createProject(
+        'Balls Board',
+        'Clear up all balls from the board',
+        'Game - Clear up all balls from the board',
+        ['Games'],
+        'projects/ball-board-start-here/index.html'))
+
+    //Add Project5
+    projs.push(createProject(
+        'Game Life',
+        'The game life',
+        'Board of life',
+        ['Games'],
+        'projects/game-life/‏‏index.html'))
+
+    //Add Project6
+    projs.push(createProject(
+        'Bookstore',
+        'Book store - Show your book',
+        'Bookstore - View books for sale with sorting by name or perice',
+        ['Games'],
+        'projects/Exercises-book-shop/index.html'))
+
+    return projs
+}
+
+function createProject(name, title, desc, labels, url = '#', publishedAt = Date.now()) {
+    return {
+        id: ++gId, name, title, desc, url, publishedAt, labels
+    }
+}
+
+function GetgProjs() {
+    return gProjs
+}
+
+function getProjectById(idProject) {
+    return gProjs.find(project => idProject === project.id)
+}
